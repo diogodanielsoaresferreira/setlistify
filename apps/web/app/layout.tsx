ba054fcfbc060421a0c859a1b4a6c755fc0ebc3b
Diogo Ferreira <diogo@timefold.ai>
2025-08-16 18:51:25 +0200

    Add Vercel analytics and SpeedInsights to the web app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,9 @@ export const metadata = {
   description: 'Create a Spotify playlist of the live songs of your favorite artist',
 };
 
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -16,6 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </header>
           {children}
         </div>
+				<Analytics />
+				<SpeedInsights />
       </body>
     </html>
   );
